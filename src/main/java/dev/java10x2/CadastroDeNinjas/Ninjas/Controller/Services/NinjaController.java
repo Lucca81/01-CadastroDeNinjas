78325565cfcc88541b458cc03a4d551e313f0898
Lucca81 <luccadev@hotmail.com>
2024-12-26 14:34:04 -0300

    build: conecta o banco de dados h2 a minha aplicacao SPRING

---
+++ src/main/java/dev/java10x2/CadastroDeNinjas/Ninjas/Controller/Services/NinjaController.java
@@ -0,0 +1,18 @@
+package dev.java10x2.CadastroDeNinjas.Ninjas.Controller.Services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+
+    }
+    /**/
+
+}
---
+++ src/main/java/dev/java10x2/CadastroDeNinjas/Ninjas/Controller/Services/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x2.CadastroDeNinjas.Ninjas.Controller.Services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
